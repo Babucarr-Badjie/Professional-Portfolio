379d08ab7600af8896445bd773fb933deee10e34
Babucarr-Badjie <babucarr.badjie12@gmail.com>
2022-11-11 21:53:30 +0800

    Updated Experience section

---
+++ index.js
@@ -0,0 +1,23 @@
+// Change Navbar on sroll
+window.addEventListener("scroll", () => {
+  document.querySelector("nav").classList.toggle("on-scroll", window.scrollY);
+});
+
+// show/hide NavBar Menu
+const menu = document.querySelector(".nav-menu");
+const menuBotton = document.querySelector("#open-menu");
+const closeBotton = document.querySelector("#close-menu");
+
+menuBotton.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBotton.style.display = "inline-block";
+  menuBotton.style.display = "none";
+});
+
+// close NavBar menu
+const closeNavBar = () => {
+  menu.style.display = "none";
+  closeBotton.style.display = "none";
+  menuBotton.style.display = "inline-block";
+};
+closeBotton.addEventListener("click", closeNavBar);
